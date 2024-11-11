658f7020f4774460f17d514b40485167f6e8e9d2
hyotatoFrappuccino <hjkiln0@gmail.com>
2024-11-11 22:18:43 +0900

    Initial commit

---
+++ src/main/java/com/studioP/noticeDuriNew/NoticeDuriNewApplication.java
@@ -0,0 +1,13 @@
+package com.studioP.noticeDuriNew;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NoticeDuriNewApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NoticeDuriNewApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/studioP/noticeDuriNew/NoticeDuriNewApplicationTests.java
@@ -0,0 +1,13 @@
+package com.studioP.noticeDuriNew;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NoticeDuriNewApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
